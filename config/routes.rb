d66eb6663b91c53214dba8dfe2f9be4a06b2df3a
aronchupa23 <naoki1194_boowy23@icloud.com>
2020-02-17 23:48:10 +0900

    投稿機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'todolists/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'top' => 'homes#top'
   # httmlメゾットtop=webサイトを閲覧するだけの時に使用
   # url 'top'にアクセスした際に"homes"コントロラーの"topアクションを呼び出す（アクションの前はシャープを使用する）"
+  post 'todolists' =>'todolists#create'
 end

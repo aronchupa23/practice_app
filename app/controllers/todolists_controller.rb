b5d2388aa1a7a2f9075e989d442703b8304fde51
aronchupa23 <naoki1194_boowy23@icloud.com>
2020-02-18 22:44:43 +0900

    投稿機能編集

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -1,10 +1,10 @@
 class TodolistsController < ApplicationController
   def new
-  	@list = List.new
+  	@list = List.new #@listはインスタンス変数 Viewに引数を渡し、表示させるために空のobjectを作成する
   end
 
   def create
-  	list = List.new(list_params)
+  	list = List.new(list_params) #インスタンスの作成(new)
   	list.save
   	redirect_to '/top'
 end
